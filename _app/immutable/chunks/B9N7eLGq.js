9a46c3e919ba1f94a4de3ad4361c12b09f32b77f
gottasaveemall <gottasaveemall@users.noreply.github.com>
2026-05-18 01:42:32 +0300

    Deploy chub-legacy catalog (static SPA)

---
+++ _app/immutable/chunks/B9N7eLGq.js
@@ -0,0 +1,1 @@
+const t="chub:lastCatalogQuery";function e(a){try{sessionStorage.setItem(t,a)}catch{}}function s(){try{return sessionStorage.getItem(t)||""}catch{return""}}export{s as l,e as s};
---
+++ _app/immutable/chunks/CaTS_1m0.js
@@ -0,0 +1,1 @@
+import{t as r}from"./BZLK0OGn.js";import{b as t}from"./CknZoCIM.js";const e={get error(){return r.error},get params(){return r.params},get route(){return r.route},get status(){return r.status},get url(){return r.url}};t.updated.check;const o=e;export{o as p};
